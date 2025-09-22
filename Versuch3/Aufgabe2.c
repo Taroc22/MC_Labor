084172c467ee7bb226e9c39559662e42195d69cf
Amir Tannouri <amirtannoury@gmail.com>
2025-09-22 21:21:56 +0200

    versuch3

---
+++ Versuch3/Aufgabe2.c
@@ -0,0 +1,39 @@
+#include <MSP430.h> //Register des Prozessors
+
+void main(void){
+
+	// WDTCTL = Watchdog Timer Control
+    // WDTPW = Watchdog Timer Password
+	WDTCTL = WDTPW + WDTHOLD;
+
+	P1DIR = BIT0;    // P1.0 als Ausgang (LED1)
+	P1REN = BIT1;    // P1.1 Pull Resistor
+    P1OUT = BIT1;    // P1.1 Pull-Up aktivieren
+	P1IES = BIT1;	 // Port 1 Interrupt Edge Select Register (Fallende Flanke da PullUp)
+	P1IE = BIT1;	 // Port 1 Interrupt Enable Register
+
+	P2REN = BIT1;    // P2.1 Pull Resistor
+    P2OUT = BIT1;    // P2.1 Pull-Up aktivieren
+	P2IES = BIT1;	 // Port 1 Interrupt Edge Select Register (Fallende Flanke da PullUp)
+	P2IE = BIT1;	 // Port 2 Interrupt Enable Register
+
+	// Globale Interruptfreigabe + Energiesparen
+	__bis_SR_register(LPM4_bits + GIE);
+
+}
+
+// ISR für Taster 1
+__attribute__((interrupt(PORT1_VECTOR)))
+void P1_VECTOR_ISR(void){
+	// LED einschalten
+	P1OUT |= BIT0;
+	P1IFG &= ~BIT1; //Bit clearen
+}
+
+// ISR für Taster 2
+__attribute__((interrupt(PORT2_VECTOR)))
+void P2_VECTOR_ISR(void){
+	// LED ausschalten
+	P1OUT &= ~BIT0;
+	P2IFG &= ~BIT1; //Bit clearen
+}
