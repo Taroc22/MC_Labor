38d10e6d0f9c80b41bf13ed101360254a18e6523
Amir Tannouri <amirtannoury@gmail.com>
2025-09-24 15:30:49 +0200

    Update Aufgabe2.c

--- Versuch3/Aufgabe2.c
+++ Versuch3/Aufgabe2.c
@@ -6,16 +6,16 @@ void main(void){
     // WDTPW = Watchdog Timer Password
 	WDTCTL = WDTPW + WDTHOLD;
 
-	P1DIR = BIT0;    // P1.0 als Ausgang (LED1)
-	P1REN = BIT1;    // P1.1 Pull Resistor
-    P1OUT = BIT1;    // P1.1 Pull-Up aktivieren
-	P1IES = BIT1;	 // Port 1 Interrupt Edge Select Register (Fallende Flanke da PullUp)
-	P1IE = BIT1;	 // Port 1 Interrupt Enable Register
+	P1DIR |= BIT0;    // P1.0 als Ausgang (LED1)
+	P1REN |= BIT1;    // P1.1 Pull Resistor
+    P1OUT |= BIT1;    // P1.1 Pull-Up aktivieren
+	P1IES |= BIT1;	 // Port 1 Interrupt Edge Select Register (Fallende Flanke da PullUp)
+	P1IE |= BIT1;	 // Port 1 Interrupt Enable Register
 
-	P2REN = BIT1;    // P2.1 Pull Resistor
-    P2OUT = BIT1;    // P2.1 Pull-Up aktivieren
-	P2IES = BIT1;	 // Port 1 Interrupt Edge Select Register (Fallende Flanke da PullUp)
-	P2IE = BIT1;	 // Port 2 Interrupt Enable Register
+	P2REN |= BIT1;    // P2.1 Pull Resistor
+    P2OUT |= BIT1;    // P2.1 Pull-Up aktivieren
+	P2IES |= BIT1;	 // Port 1 Interrupt Edge Select Register (Fallende Flanke da PullUp)
+	P2IE |= BIT1;	 // Port 2 Interrupt Enable Register
 
 	// Globale Interruptfreigabe + Energiesparen
 	__bis_SR_register(LPM4_bits + GIE);
