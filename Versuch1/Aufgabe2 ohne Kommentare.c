79a373f9b02d854846d4c54ceb65b6ea4737bc33
Amir Tannouri <amirtannoury@gmail.com>
2025-09-25 13:45:44 +0200

    Update Aufgabe2 ohne Kommentare.c

--- Versuch1/Aufgabe2 ohne Kommentare.c
+++ Versuch1/Aufgabe2 ohne Kommentare.c
@@ -2,18 +2,17 @@
 
 void main(void)
 {
-    // Init 
-    WDTCTL = WDTPW + WDTHOLD; // Watchdog aus!
+    WDTCTL = WDTPW + WDTHOLD;
 
-    P1DIR |= BIT0;    // P1.0 als Ausgang (LED1)
-    P1REN |= BIT1;    // P1.1 Pull Resistor
-    P1OUT |= BIT1;    // P1.1 Pull-Up aktivieren
+    P1DIR |= BIT0;
+    P1REN |= BIT1;  
+    P1OUT |= BIT1;  
 
-    P2DIR = 0x00;    // P2.x Eingänge (Eigentlich überflüssig)
-    P2REN |= BIT1;    // P2.1 Pull Resistor
-    P2OUT |= BIT1;    // P2.1 Pull-Up aktivieren
+    P2DIR = 0x00; 
+    P2REN |= BIT1;   
+    P2OUT |= BIT1;   
 
-    P4DIR |= BIT7;    // P4.7 als Ausgang (LED2)
+    P4DIR |= BIT7;   
 
     unsigned int lastState1 = (P2IN & BIT1);
     unsigned int lastState2 = (P1IN & BIT1);
@@ -23,14 +22,13 @@ void main(void)
         unsigned int nowP1 = (P2IN & BIT1);
         unsigned int nowP2 = (P1IN & BIT1);
 
-        // Flanke High -> Low (Taste gedrückt wegen PullUp)
         if (lastState1 && !nowP1) {
-            P1OUT ^= BIT0;   // LED1 toggeln
+            P1OUT ^= BIT0; 
         }
         lastState1 = nowP1;
 
         if (lastState2 && !nowP2) {
-            P4OUT ^= BIT7;   // LED2 toggeln
+            P4OUT ^= BIT7; 
         }
         lastState2 = nowP2;
     }
