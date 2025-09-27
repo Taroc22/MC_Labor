b2951e72256c1ed3265f037509ce49904f285021
Amir Tannouri <amirtannoury@gmail.com>
2025-09-27 19:37:03 +0200

    snake

--- Snake/main.c
+++ Snake/main.c
@@ -6,6 +6,7 @@
 #include <msp430.h>
 #include "ST7735.h"
 #include <string.h>
+#include <stdint.h>
 
 #define sb(reg, bit) ((reg) |= (bit))
 #define cb(reg, bit) ((reg) &= ~(bit))
@@ -24,6 +25,8 @@
 #define DISPLAY_WIDTH 128
 #define DISPLAY_HEIGHT 128
 
+#define ROWS 10
+#define COLS 10
 
 /*
     Anzahl Zeilen (Höhe): 0-10
@@ -32,17 +35,22 @@
     Timer A0 = delay
 */
 
-struct RegBit {
-    volatile unsigned char *reg;
-    unsigned char bit;
+uint8_t field[ROWS][COLS];
+
+enum RegType { REG_BIT, REG_VAL };
+
+struct RegOp {
+    enum RegType type;
+    volatile void *reg;
+    unsigned int value;
 };
 
-
-struct RegBit regBits[] = {
-    { &P1REN, BIT1 },
-    { &P1OUT, BIT1 },
-    { &P2REN, BIT1 },
-    { &P2OUT, BIT1 }
+struct RegOp ops[] = {
+    { REG_BIT, &P1REN, BIT1 },
+    { REG_BIT, &P1OUT, BIT1 },
+    { REG_BIT, &P2REN, BIT1 },
+    { REG_BIT, &P2OUT, BIT1 }
+    //{ REG_VAL, &TA0CTL, TASSEL_1 + MC_1 + ID_3 + TACLR } Beispiel
 };
 
 
@@ -61,14 +69,24 @@ int centerText(const char *text) {
     return (DISPLAY_WIDTH - width) / 2;
 }
 
+void initMCU(){
+    WDTCTL = WDTPW + WDTHOLD;
+    for (int i = 0; i < sizeof(ops)/sizeof(ops[0]); i++) {
+        if (ops[i].type == REG_BIT) {
+            sb(*(volatile unsigned char*)ops[i].reg, (unsigned char)ops[i].value);
+        } else if (ops[i].type == REG_VAL) {
+            *(volatile unsigned int*)ops[i].reg = ops[i].value;
+        }
+    }
+    for (int i = 0; i < ROWS; i++){
+        for (int j = 0; j < COLS; j++){
+            field[i][j] = 0;
+        }
+    }
+}
 
 void setup(){
-    WDTCTL = WDTPW + WDTHOLD;
-
-    for(int i = 0; i< 4; i++){
-        sb(*regBits[i].reg, regBits[i].bit);
-    }   
-
+    initMCU();
     ST7735_interface_init();  
     ST7735_display_init();
     
@@ -85,7 +103,7 @@ void setup(){
 
 
 void start(){
-     while(1){ //Durch Button IR ersetzen
+    while (!(P1IFG & BIT1) && !(P2IFG & BIT1)){
         setText(centerText(ST), 58, ST, WHITE, BG);
         //Hier Buzzer anschalten
         delay(600);
@@ -93,6 +111,9 @@ void start(){
         //Hier Buzzer ausschalten
         delay(600);
     }
+    draw(0, 0, 128, 128, BG);
+    if (P1IFG & BIT1) P1IFG &= ~BIT1;
+    if (P2IFG & BIT1) P2IFG &= ~BIT1;
 }
 
 
